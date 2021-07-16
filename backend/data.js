5fc63bca30530d7726c3128ec200ce26d4b26d41
Osama <osama@LAPTOP-VFNKC57J>
2021-07-16 17:06:23 +0500

    commit

---
+++ backend/data.js
@@ -0,0 +1,84 @@
+const data={
+    products:[
+        {
+            _id:'1',
+            name:'Toyota Corolla',
+            category:'Sedan',
+            image:'/images/corolla1.jpg',
+            price: 3500,
+            countInStock: 1,
+            brand: 'Toyota',
+            rating: 4.5,
+            numReviews: 10,
+            description:'Rides Smoothly. No faults. Nice and Clean.',
+
+        },
+        {
+            _id:'2',
+            name:'Toyota Vitz',
+            category:'HatchBack',
+            image:'/images/vitz.jpg',
+            price: 3000,
+            countInStock: 0,
+            brand: 'Toyota',
+            rating: 4.5,
+            numReviews: 15,
+            description:'Rides Smoothly. No faults. Nice and Clean.',
+
+        },
+        {
+            _id:'3',
+            name:'Toyota Prado',
+            category:'SUV',
+            image:'/images/prado.jpg',
+            price: 8000,
+            countInStock: 1,
+            brand: 'Toyota',
+            rating: 4.8,
+            numReviews: 8,
+            description:'Rides Smoothly. No faults. Nice and Clean.',
+
+        },
+        {
+            _id:'4',
+            name:'Toyota Supra',
+            category:'Super Car',
+            image:'/images/supra.jpg',
+            price: 15000,
+            countInStock: 1,
+            brand: 'Toyota',
+            rating: 5,
+            numReviews: 100,
+            description:'Rides Smoothly. No faults. Nice and Clean.',
+
+        },
+        {
+            _id:'5',
+            name:'Toyota Hiace',
+            category:'Van',
+            image:'/images/hiace.jpg',
+            price: 5000,
+            countInStock: 0,
+            brand: 'Toyota',
+            rating: 3.5,
+            numReviews: 14,
+            description:'Rides Smoothly. No faults. Nice and Clean.',
+
+        },
+        {
+            _id:'6',
+            name:'Toyota Coaster',
+            category:'Bus',
+            image:'/images/coaster.jpg',
+            price: 10000,
+            countInStock: 1,
+            brand: 'Toyota',
+            rating: 4.5,
+            numReviews: 69,
+            description:'Rides Smoothly. No faults. Nice and Clean.',
+
+        },
+    ],
+};
+
+export default data;
