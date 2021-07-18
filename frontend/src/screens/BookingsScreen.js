af8d7f9c82377cb9f32af65a3bab9c13d28360de
Osama <osama@LAPTOP-VFNKC57J>
2021-07-18 18:56:35 +0500

    commit

--- frontend/src/screens/BookingsScreen.js
+++ frontend/src/screens/BookingsScreen.js
@@ -1,24 +1,85 @@
 import React, { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
 import { addToBooking } from '../actions/bookingActions';
+import MessageBox from '../components/MessageBox';
 
 export default function BookingsScreen(props) {
     const productId = props.match.params.id;
     const day = props.location.search ? Number(props.location.search.split('=')[1]) : 1;
+    const booking = useSelector(state => state.booking);
+    const { bookingItems } = booking;
     const dispatch = useDispatch()
     useEffect(() => {
         if (productId) {
             dispatch(addToBooking(productId, day));
         }
     }, [dispatch, productId, day]);
+    const removeFromBookingHandler = (id) => {
+        //delete action
+    };
+    const confirmbookingHandler = () => {
+        props.history.push('/signin?redirect=confirmedBookings');
+    }
     return (
-        <div>
-            <h1>
-                Bookings Screen
-            </h1>
-            <p>
-                CURRENT UNCONIFIRMED BOOKINGS: ProductID: {productId} Booking for: {day} day/days
-            </p>
+        <div className="row top">
+            <div className="col-2">
+                <h1>Unconfirmed Bookings List</h1>
+                {bookingItems.length === 0 ? <MessageBox>
+                    No Unconfirmed Bookings.<Link to="/">Start Renting :)</Link>
+                </MessageBox>
+                    :
+                    (
+                        <ul>
+                            {
+                                bookingItems.map((item) => (
+                                    <li key={item.product}>
+                                        <div className="row">
+                                            <div>
+                                                <img src={item.image} alt={item.name} className="small"></img>
+                                            </div>
+                                            <div className="min-30">
+                                                <Link to={`/product/${item.product}`}>{item.name}</Link>
+                                            </div>
+                                            <div>
+                                                <select value={item.day} onChange={e => dispatch(addToBooking(item.product, Number(e.target.value)))}>
+                                                    {
+                                                        [...Array(15).keys()].map(x => (
+                                                            <option key={x + 1} value={x + 1}>{x + 1} </option>
+                                                        ))
+                                                    }
+                                                </select>
+                                            </div>
+                                            <div>
+                                                PKR {item.price} per day
+                                            </div>
+                                            <div>
+                                                <button type="button" onClick={() => removeFromBookingHandler(item.product)}>Delete</button>
+                                            </div>
+
+                                        </div>
+                                    </li>
+                                ))
+                            }
+                        </ul>
+                    )
+                }
+            </div>
+            <div className="col-1">
+                <div className="card card-body">
+                    <ul>
+                        <li>
+                            <h2>
+                                Subtotal ({bookingItems.reduce((a, c) => a + c.day, 0)} items) : PKR {bookingItems.reduce((a, c) => a + c.price * c.day, 0)}
+                            </h2>
+                        </li>
+                        <li>
+                            <button type="button" onClick={confirmbookingHandler} className="primary block" disabled={bookingItems.length === 0}>
+                                Complete All Bookings </button>
+                        </li>
+                    </ul>
+                </div>
+            </div>
         </div>
     )
 }
