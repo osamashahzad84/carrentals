2a8fc14917dd9d00a3e18cf53fd7a1bb1e16eb42
Osama <osama@LAPTOP-VFNKC57J>
2021-07-30 18:16:45 +0500

    commit

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import path from 'path';
 import userRouter from './routers/userRouter.js';
 import vehicleRouter from './routers/vehicleRouter.js';
 import bookingRouter from './routers/bookingRouter.js';
+import uploadRouter from './routers/uploadRouter.js';
 
 dotenv.config();
 const app = express();
@@ -15,6 +17,7 @@ mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/carrentals', {
     useUnifiedTopology: true,
     useCreateIndex: true,
 })
+app.use('/api/uploads', uploadRouter)
 
 app.use('/api/users', userRouter);
 
@@ -25,6 +28,9 @@ app.use('/api/bookings', bookingRouter);
 /*app.get('/api/config/easypaisa',(req,res)=> {
     res.send(process.env.EASYPAISA_CLIENT_ID || 'sb' ) //sb means sandbox
 })*/
+
+const __dirname = path.resolve();
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
 app.get('/', (req, res) => {
     res.send('Server is ready');
--- frontend/src/screens/VehicleEditScreen.js
+++ frontend/src/screens/VehicleEditScreen.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux';
+import Axios from 'axios';
 import { detailsVehicle, updateVehicle } from '../actions/vehicleActions';
 import LoadingBox from '../components/LoadingBox';
 import MessageBox from '../components/MessageBox';
@@ -49,6 +50,33 @@ export default function VehicleEditScreen(props) {
         })
         )
     }
+
+    const [loadingUpload, setLoadingUpload] = useState(false);
+    const [errorUpload, setErrorUpload] = useState('');
+
+    const userSignin = useSelector(state => state.userSignin);
+    const { userInfo } = userSignin;
+
+    const uploadFileHandler = async (e) => {
+        const file = e.target.files[0];
+        const bodyFormData = new FormData();
+        bodyFormData.append('image', file);
+        setLoadingUpload(true);
+        try {
+            const { data } = await Axios.post('/api/uploads', bodyFormData, {
+                headers: {
+                    'Content-Type': 'multipart/form-data',
+                    Authorization: `Bearer ${userInfo.token}`
+                }
+            })
+            setImage(data);
+            setLoadingUpload(false);
+        } catch (error) {
+            setErrorUpload(error.message);
+            setLoadingUpload(false);
+        }
+    }
+
     return (
         <div>
             <form className="form" onSubmit={submitHandler}>
@@ -76,6 +104,13 @@ export default function VehicleEditScreen(props) {
                                 <label htmlFor="image">Image</label>
                                 <input id="image" type="text" placeholder="Enter image"
                                     value={image} onChange={(e) => setImage(e.target.value)}></input>
+                            </div>
+                            <div>
+                                <label htmlFor="imageFile">Image File</label>
+                                <input type="file" id="imageFile" label="Choose Image"
+                                    onChange={uploadFileHandler}></input>
+                                {loadingUpload && <LoadingBox></LoadingBox>}
+                                {errorUpload && (<MessageBox variant="danger">{errorUpload}</MessageBox>)}
                             </div>
                             <div>
                                 <label htmlFor="category">Category</label>
